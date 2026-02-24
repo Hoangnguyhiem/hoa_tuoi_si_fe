ea2e0f9d945e8ba001b0d15ace53399037870d07
Hoangnguyhiem <vinsomatem97@gmail.com>
2026-02-25 05:51:10 +0700

    update 24/2 three

--- src/pages/(dashboard)/product/page.tsx
+++ src/pages/(dashboard)/product/page.tsx
@@ -22,10 +22,14 @@ const ProductPage = () => {
 			backgroundColor: "#ffffff"
 		});
 
-		const link = document.createElement("a");
-		link.download = `don-hang-${id}.png`;
-		link.href = dataUrl;
-		link.click();
+		if (/Android|iPhone/i.test(navigator.userAgent)) {
+			window.open(dataUrl, "_blank");
+		} else {
+			const link = document.createElement("a");
+			link.download = `don-hang-${id}.png`;
+			link.href = dataUrl;
+			link.click();
+		}
 	};
 
 	// copy ảnh
@@ -55,8 +59,6 @@ const ProductPage = () => {
 		}
 	})
 
-	console.log(order);
-	
 
 	return (
 		<div className="bg-slate-100 h-screen">
@@ -75,8 +77,8 @@ const ProductPage = () => {
 											<div className="mb-[10px]">
 												<div className="flex justify-between">
 													<div className="font-[600] text-[17px] mb-[5px] lg:flex">
-														<span className="lg:mr-[10px]">{item?.userId?.name}</span>
-														<span className="flex lg:border-l-[1px] lg:border-slate-300 lg:ml-[10px]">
+														<span className="lg:pr-[10px]">{item?.userId?.name}</span>
+														<span className="flex lg:border-l-[1px] lg:border-slate-300 lg:pl-[10px]">
 															{item?.userId?.phone}
 														</span>
 													</div>
@@ -172,7 +174,7 @@ const ProductPage = () => {
 							<hr />
 							<div className="flex justify-center">
 								<div className="flex m-[8px]">
-									<div onClick={() => handleDownload(item._id)} className="flex mr-[100px] cursor-pointer">
+									<div onClick={() => handleDownload(item._id)} className="flex md:mr-[100px] cursor-pointer">
 										<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
 											<path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
 										</svg>
@@ -180,7 +182,7 @@ const ProductPage = () => {
 											Tải ảnh
 										</button>
 									</div>
-									<div onClick={() => handleCopy(item._id)} className="flex cursor-pointer">
+									<div onClick={() => handleCopy(item._id)} className="cursor-pointer hidden md:flex">
 										<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
 											<path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
 										</svg>
