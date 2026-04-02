8a64c107fe342ff8d20e142e3b4454eba1a7ca30
Hoangnguyhiem <vinsomatem97@gmail.com>
2026-04-02 11:21:09 +0700

    2/4 six

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: "https://hoa-tuoi-si.onrender.com/api"
+    baseURL: `${import.meta.env.VITE_API_URL}/api`
     // baseURL: "http://localhost:8080/api"
 })
 export default instance
